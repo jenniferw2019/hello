dc18744ef434618687b369f5f8ba791ba6c64ee0
jenniferw2019 <90578300+jenniferw2019@users.noreply.github.com>
2021-09-12 14:23:39 -0700

    Add files via upload
    test upload

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+// this is a hello world c++ program
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+  cout << "hello world" << endl;
+  // int input = 0;
+  char input = 'a';
+  cin >> input;
+  cout << input << endl;
+  return 0;
+}
